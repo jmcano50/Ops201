52cbf376c649ff9b32cc06c7b89ebd58a1f1a323
Juan Miguel <jcano@aol.com>
2023-10-24 16:52:10 -0700

    helloworld

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script Name:                  helloworld.sh
+# Author:                       Juan Miguel Cano
+# Date of latest revision:      10/24/2023
+# Purpose:                      To say hello to the world
+# Declaration of variables
+# Declaration of functions
+# Main
+echo "Hello World!"
+echo "I am working perfectly"
+# End
