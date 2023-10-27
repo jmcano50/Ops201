26c11eecf9fd9b684aede44e13e540f10de97de8
jmcano50 <jcano@aol.com>
2023-10-26 21:45:11 -0700

    array.sh

---
+++ array.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script Name:		    	        array.sh
+# Author Name:			            Juan Miguel Cano
+# Date of latest revision:      	10/26/2023
+# Purpose: 			                Ops Challange Class 03 Functions
+
+# Declaration of functions
+
+Marine="LeatherNeck"
+
+Marines=("LeatherNeck" "JarHead" "DevilDog" "CrayonEater" "Gunny")
+
+# Marines = []
+
+# Declaration of functions
+
+echo ${Marines[0]}
+echo ${Marines[4]}
+
+echo $Marine 
+
