ee177dc82138b4f754551f81109513ab5e545898
jmcano50 <jcano@aol.com>
2023-10-26 22:03:57 -0700

    Ops4.sh

---
+++ Ops4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create four directories
+mkdir dir1 dir2 dir3 dir4
+
+# Store the directory paths in an array
+directories=("dir1" "dir2" "dir3" "dir4")
+
+# Loop through the array and create a new .txt file in each directory
+for dir in "${directories[@]}"; do
+    touch "$dir/$dir.txt"
+    echo "Created $dir/$dir.txt"
+done
+
+echo "Script completed."
